4a6b1cfea08d5d9b82251830989b26bbc4f3771c
hopo <hpsight@gmail.com>
2017-02-22 20:57:48 +0900

    -

--- math.js
+++ math.js
@@ -1,15 +1,11 @@
 function toBin(n){
   var bin = n.toString(2);
-  console.log(n+" to bin: "+bin);
   return bin;
 }
 
-toBin(14);
+console.log('bin: '+toBin(14));
 
- //
- // function binval(i){
- //   return Math.pow(2, i);
- // }
- //
- // console.log(binval(4));
- //
+console.log('AND: ', 1 & 1); // AND
+console.log('OR:  ', 1 | 1); // OR
+console.log('XOR: ', 1 ^ 1); // XOR
+console.log(~ 1); // NOT
